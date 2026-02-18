c4b0bea3e925668ac008eead2224ce912f565f4d
Your Name <your-email@example.com>
2026-02-17 22:39:16 -0500

    coach mark home page5/5

--- src/tour/CoachMarkLayer.tsx
+++ src/tour/CoachMarkLayer.tsx
@@ -73,7 +73,7 @@ export const CoachMarkLayer: React.FC = () => {
       : step?.id === 'home-stats-box'
         ? 19
         : step?.id === 'home-menu-item'
-          ? -2
+          ? 6
           : step?.id === 'home-profile-restaurant'
             ? 14
             : 10;
